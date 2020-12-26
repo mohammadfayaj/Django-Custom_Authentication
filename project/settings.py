0ab6b6c1bb36783571935e6e718003c271ef03f3
mohammadfayaj <mohammadfayaj36@gmail.com>
2020-12-26 14:05:30 +0600

    this is commit

--- project/settings.py
+++ project/settings.py
@@ -132,10 +132,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'blog:blog-home'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
@@ -143,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mmsssohammadfayaj36@gmail.com'
-EMAIL_HOST_PASSWORD = 'sonofgun36'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password7441564'
